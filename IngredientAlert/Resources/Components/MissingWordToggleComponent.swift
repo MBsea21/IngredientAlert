b494196ab64b4919c97a05f2c22e0408b09a36e6
Mbsea21 <madeline.bennett.pm@gmail.com>
2025-02-06 23:25:06 -0800

    working on home page- doing a commit before updating state management of ingredient dict & productFE model.

---
+++ IngredientAlert/Resources/Components/MissingWordToggleComponent.swift
@@ -0,0 +1,67 @@
+//
+//  ToggleComponent.swift
+//  IngredientAlert
+//
+//  Created by Madeline Bennett on 2/5/25.
+//
+import SwiftUI
+
+struct WordToggleView: View {
+    @EnvironmentObject modelData : ModelData
+    var word: String
+    @State var text: String = ""
+    @State var sourceUrl: String =  ""
+    @State var pubChemUrl: String = ""
+    @State var isFlagged: Bool = false
+    @State var isCommon: Bool = false
+    
+    
+    @State private var wordToggle = false
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Toggle(word, isOn: $wordToggle)
+                .padding(.horizontal)
+            
+            if wordToggle == true {
+                Text("Add \(word)")
+                ScrollView{
+                    Toggle("Is Common Name", isOn: $isCommon)
+                    if isCommon == true {
+                        let commonName = word
+                    }
+                    InputView(text:$text, title: "name", placeholder: "string ")
+                    InputView(text:$sourceUrl, title: "SourceURL", placeholder:"www.pubMed.com")
+                    InputView(text:$pubChemUrl, title:"Chem URL", placeholder:"www.pubchem.com")
+                    Toggle("IsFlagged", isOn: $isFlagged)
+                    
+                    Button{
+                        Task {
+                            addCommonIngredient()
+                        }
+                    } label: {
+                        HStack {
+                            Text("Add Common Ingredient")
+                                .fontWeight(.semibold)
+                            Image(systemName: "arrow.right")
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 350, height: 48)
+                    }
+                    .background(Color(.systemBlue))
+                    .cornerRadius(10)
+                    .padding(.top, 24)
+                }
+            }
+        }
+    }
+        private func addCommonIngredient () {
+            modelData.ingredientListViewModel.addCommon(inputName: text,
+                                                        commonName: text,
+                                                        isCommonName: true,
+                                                        isFlagged: isFlagged,
+                                                        sourceUrl: sourceUrl,
+                                                        pubChemUrl: pubChemUrl )
+    }
+                    
+}
